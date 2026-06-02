8fc3dcf9c73ddeabe26e827b40a5e89dd9619b11
mds-webdev <webdev.mds@gmail.com>
2026-06-03 01:21:37 +0200

    Update last

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://fv-oberstenfeld-gronau.de',
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap({
+      filter: (page) => !page.includes('/mitglieder/'),
+    }),
+  ],
 });
